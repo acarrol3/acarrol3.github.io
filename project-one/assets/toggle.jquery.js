778046ac59b0fc7c9be362b0b39a1b02b8635512
Alyssa Carroll <acarrol3@nd.edu>
2019-10-01 19:49:20 -0400

    final final

--- project-one/assets/toggle.jquery.js
+++ project-one/assets/toggle.jquery.js
@@ -2,11 +2,11 @@ $(document).ready(function (){
     //ID or Class name of the button goes into the first section
     $('#button1').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#letter-analysis").removeClass("invisible");
+        $(".letter-analysis").removeClass("invisible");
         $("#arrow").addClass("invisible");
         $("#large-letter-analysis").removeClass("invisible");
         $("#large-letter").addClass("invisible");
-        $("#button2").removeClass("invisible");
+        // $("#button2").removeClass("invisible");
         $("#button1").addClass("invisible");
         });
 
@@ -14,9 +14,9 @@ $(document).ready(function (){
 
 $(document).ready(function (){
     //ID or Class name of the button goes into the first section
-    $('#button2').click(function() {
+    $('#close-button').click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#letter-analysis").addClass("invisible");
+        $(".letter-analysis").addClass("invisible");
         $("#arrow").removeClass("invisible");
         $("#large-letter-analysis").addClass("invisible");
         $("#large-letter").removeClass("invisible");
